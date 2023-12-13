8f1d7bfc9845eb7f1c95b8f7962f62d28a341492
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-12-13 13:28:32 +0100

    first commit

---
+++ database/migrations/2023_12_13_122252_create_trains_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code');
+            $table->integer('number_of carriages');
+            $table->boolean('in_time')->default(true);
+            $table->boolean('deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
